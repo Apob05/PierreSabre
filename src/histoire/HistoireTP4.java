59f2327841675dbd1b112fe49b83702fa01e2efa
Apob05 <129179145+Apob05@users.noreply.github.com>
2024-03-29 08:59:31 +0100

    Commerçant

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,10 +1,15 @@
 package histoire;
 
+import personnages.Commercant;
+
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Commercant marcos = new Commercant("Marcos", 20);
+		marcos.direBonjour();
+		marcos.seFaireExtorquer();
+		marcos.recevoir(15);
+		marcos.boire();
 	}
 
 }
