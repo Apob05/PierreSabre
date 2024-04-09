6c4de63bec981eb80e4386b896c78aed8d62caf7
Apob05 <129179145+Apob05@users.noreply.github.com>
2024-04-09 08:08:36 +0200

    petite modif

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -7,7 +7,7 @@ public class HistoireTP4 {
 
 	public static void main(String[] args) {
 		Yakuza yaku = new Yakuza("Yaku le Noir", "whisky", 15, "Warsong");
-		Ronin roro = new Ronin("Roro", "shochu", 60);
+		Ronin roro = new Ronin("Roro", "shochu", 54);
 		roro.parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fait à ce pauvre marchand!");
 		roro.provoquer(yaku);
 
